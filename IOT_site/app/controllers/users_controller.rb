3b28c4e09918eda29e2130cdfc4f46a00b6bff95
Arthur <arthur.bucker@hotmail.com>
2019-11-22 18:09:24 -0300

    search fixed

--- IOT_site/app/controllers/users_controller.rb
+++ IOT_site/app/controllers/users_controller.rb
@@ -65,10 +65,14 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+    if !super_user
+      redirect_to home_path
+    else
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
   end
 
